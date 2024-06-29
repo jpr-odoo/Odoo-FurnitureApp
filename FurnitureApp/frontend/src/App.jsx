3919383bff1d245e5249e2db387579f345cab409
Piyush <piyushbhagchandani.64@gmail.com>
2024-06-29 17:30:04 +0530

    header

--- FurnitureApp/frontend/src/App.jsx
+++ FurnitureApp/frontend/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import HeroSectionMain from "./components/HeroSectionMain"
 import Categories from './components/Categories';
 import Footer from './components/Footer';
-import Header from "./comp./components/Header
+import Header from "./components/Header"
 import FurnitureCard from "./components/FurnitureCard"
 import KnowMore from './components/KnowMore';
 
